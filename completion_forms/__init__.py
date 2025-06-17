05ee5cea0617bd216e1805b6d578fb3b233e18e0
capacap <simon.sorkin@outlook.com>
2025-06-17 16:20:24 +0200

    dependencies now managed via pyproject

--- completion_forms/__init__.py
+++ completion_forms/__init__.py
@@ -1,8 +1,30 @@
 from .client import CompletionClient, CompletionClientSettings
+from .exceptions import (
+    ClientConfigurationError,
+    FormFileNotFoundError,
+    FormValidationError,
+    InvalidJSONError,
+    InvalidKeyError,
+    InvalidTemplateError,
+    InvalidValueError,
+    MaxRetriesExceededError,
+    ReservedKeyError,
+    ResponseParsingError,
+)
 from .form import CompletionForm
 
 __all__ = [
+    "CompletionForm",
     "CompletionClient",
     "CompletionClientSettings",
-    "CompletionForm",
+    "FormFileNotFoundError",
+    "InvalidJSONError",
+    "InvalidTemplateError",
+    "ReservedKeyError",
+    "InvalidKeyError",
+    "InvalidValueError",
+    "FormValidationError",
+    "ResponseParsingError",
+    "ClientConfigurationError",
+    "MaxRetriesExceededError",
 ] 
